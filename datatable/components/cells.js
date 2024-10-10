3ab74c5843ec0bd539df45a2bf357180a271fc3c
Youssef-alTai-JS-appy <youssef_al_tai_js@appyinnovator.com>
2024-10-10 14:38:56 +0300

    Fix table row colors

--- datatable/components/cells.js
+++ datatable/components/cells.js
@@ -10,13 +10,13 @@ const statusStyle = (status) => {
 };
 
 const digitalSignatureCell = (digitalSignature) => `
-  <span class="px-4 py-2 rounded-full bg-gray-100 text-gray-500 text-xs">
+  <span class="px-4 py-2 rounded-full border border-gray-200  bg-gray-100 text-gray-500 text-xs">
     ${digitalSignature}
   </span>
 `;
 
 const expiryDateCell = (expiryDate) => `
-  <div class="flex items-center gap-2 text-gray-500 text-xs border border-gray-200 rounded-full px-4 py-2 w-min">
+  <div class="flex items-center gap-2 bg-white text-gray-500 text-xs border border-gray-200 rounded-full px-4 py-2 w-min">
       ${calendarIcon}
     ${expiryDate}
   </div>
